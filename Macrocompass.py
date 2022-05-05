0fa7aef8dfd7f3eea4bba35c6011b872ddf32c4a
Cober <67133227+caiquecober@users.noreply.github.com>
2022-05-05 13:46:18 -0300

    Update Macrocompass.py

--- Macrocompass.py
+++ Macrocompass.py
@@ -201,8 +201,8 @@ def get_series(id_selected):
 
 #Options headers
 col1, col2,_, col3 = st.columns((2,2,1,2))
-fred_code = col1.text_input('FRED code', value="CPILFESL")
-titulo = col2.text_input('Chart Title', value="US inflation - ex energy and food")
+fred_code = col1.text_input('##**FRED code**', value="CPILFESL")
+titulo = col2.text_input('##**Chart Title**', value="US inflation - ex energy and food")
 
 st.markdown(html_line_2, unsafe_allow_html=True)
 
